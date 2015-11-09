9f2fd3059b20fa82553995e1477b8292c3c7c651
Joseph Matthias Goh <joseph@zephinzer.com>
2015-11-09 19:56:03 +0800

    Added subtract and modified lucas kanade.

--- mac.bgsubtract.py
+++ mac.bgsubtract.py
@@ -14,18 +14,7 @@ class BackgroundSubtractor:
         self.pathToVideo = inputPath
         self.imageBackground = imageBackground
         self.pathToResult = outputPath
-    def equalizeValues(self, img):
-        h = np.zeros([len(img), len(img[0])])
-        s = np.zeros([len(img), len(img[0])])
-        v = np.zeros([len(img), len(img[0])])
-        hsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
-        cv2.split(hsv, [h,s,v])
-        v = cv2.equalizeHist(cv2.convertScaleAbs(v)).astype('float32')
-        s = s.astype('float32')
-        h = h.astype('float32')
-        cv2.merge([h,s,v], hsv)
-        img = cv2.cvtColor(img, cv2.COLOR_HSV2BGR)
-        return img
+
     def readVideo(self, inputPath):
         cap = cv2.VideoCapture(inputPath)
         frameWidth = int(cap.get(cv2.cv.CV_CAP_PROP_FRAME_WIDTH))
@@ -39,15 +28,13 @@ class BackgroundSubtractor:
         bgimage = cv2.imread(self.imageBackground)
         cap,w,h,r,fc,fcc = self.readVideo(self.pathToVideo)
         wrt = cv2.VideoWriter(self.pathToResult, cv2.cv.CV_FOURCC(*fourCC), r, (w,h))
-        for fr in range(1, 100):
+        for fr in range(1, fc):
+            print fr/float(fc)*100,'% completed'
             _, img = cap.read()
-            #mask = fgmask.apply(img, mask, 1)
             frame = cv2.subtract(img, bgimage)
-            self.equalizeValues(frame)
-            frame = cv2.filter2D(frame, -1, np.ones((3,3),np.float32)/8)
             wrt.write(frame)
         cap.release()
         wrt.release()
 
-driver = BackgroundSubtractor('input.avi','bg.png','input-no-bg.mov')
-driver.run("mp4v")
+driver = BackgroundSubtractor('input-v-eq-final.avi','bg.png','input-no-bg.avi')
+driver.run("MP42")
--- mac.lucaskanade.py
+++ mac.lucaskanade.py
@@ -7,39 +7,10 @@ import os, sys
 sys.path.append('/usr/local/lib/python2.7/site-packages')
 import cv2, numpy as np
 from matplotlib import pyplot
+from macclickscaptor import ClicksCaptor
 
-class ClicksCaptor:
-    FIELD_DISPLAY_NAME = 'Field'
-    coords = []
-    nClicks = 0
-    nMaxClicks = None
-
-    def __init__(self, maxClicks):
-        self.nMaxClicks = maxClicks
-
-    ## get the videoCoords with Clicks
-    def getClick(self, e, x, y, f, p):
-        if e == cv2.EVENT_LBUTTONDOWN:
-            print x, y
-            self.coords.append([[np.float32(x), np.float32(y)]])
-            print self.nClicks
-            self.nClicks += 1
-            if(self.nClicks == self.nMaxClicks):
-                cv2.destroyWindow(self.FIELD_DISPLAY_NAME)
-
-    def getCoordsByClick(self, image):
-        cv2.imshow(self.FIELD_DISPLAY_NAME, image)
-        cv2.setMouseCallback(self.FIELD_DISPLAY_NAME, self.getClick, 0)
-        #  Press "Escape button" to exit
-        while True:
-            key = cv2.waitKey(10) & 0xff
-            if key == 27 or self.nClicks >= self.nMaxClicks:
-                break
-
-    def getResults(self):
-        return self.coords
-
-cap = cv2.VideoCapture('input.avi');
+cap = cv2.VideoCapture('input-no-bg.avi');
+cap2 = cv2.VideoCapture('input.avi');
 
 frameWidth = int(cap.get(cv2.cv.CV_CAP_PROP_FRAME_WIDTH))
 frameHeight = int(cap.get(cv2.cv.CV_CAP_PROP_FRAME_HEIGHT))
@@ -55,28 +26,31 @@ feature_params = dict( maxCorners = 100,
                        blockSize = 7 )
 
 # Parameters for lucas kanade optical flow
-lk_params = dict( winSize  = (15,15),
-                  maxLevel = 1,
+lk_params = dict( winSize  = (50,25),
+                  maxLevel = 2,
                   criteria = (cv2.TERM_CRITERIA_EPS | cv2.TERM_CRITERIA_COUNT, 10, 0.03))
 
 # Take first frame and find corners in it
 ret, old_frame = cap.read()
+_,framewbg = cap2.read()
 old_gray = cv2.cvtColor(old_frame, cv2.COLOR_BGR2GRAY)
-p0 = cv2.goodFeaturesToTrack(old_gray, mask = None, **feature_params)
+#p0 = cv2.goodFeaturesToTrack(old_gray, mask = None, **feature_params)
 #blue players first 11 = 11
 #red players second 11 = 22
 #referee last = 23
 #football last = 24
-clicksCaptor = ClicksCaptor(2)
-clicksCaptor.getCoordsByClick(old_frame)
+clicksCaptor = ClicksCaptor(8)
+clicksCaptor.getCoordsByClick(framewbg)
 p0 = np.array(clicksCaptor.getResults())
 # Create a mask image for drawing purposes
 mask = np.zeros_like(old_frame)
 
 for fr in range(1, frameCount):
     ret,frame = cap.read()
+    _,framewbg = cap2.read()
+
     frame_gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-    frame_gray = cv2.GaussianBlur(frame_gray, (5,5), 0)
+    #frame_gray = cv2.GaussianBlur(frame_gray, (5,5), 0)
     # calculate optical flow
     p1, st, err = cv2.calcOpticalFlowPyrLK(old_gray, frame_gray, p0, None, **lk_params)
     # Select good points
@@ -88,13 +62,11 @@ for fr in range(1, frameCount):
         c,d = old.ravel()
         cv2.line(   mask, (a,b),    (c,d),  [0, 0, 255],    2)
         cv2.circle( frame,(a,b),    5,      [0, 0, 255],    -1)
-    img = cv2.add(frame, mask)
-    if img != None:
-        cv2.imshow('frame',img)
-        while True:
-            k = cv2.waitKey(30) & 0xff
-            if k == 27:
-                break
+    img = cv2.add(framewbg, mask)
+    cv2.imshow('frame',img)
+    k = cv2.waitKey(30) & 0xff
+    if k == 27:
+        break
     # Now update the previous frame and previous points
     old_gray = frame_gray.copy()
     p0 = good_new.reshape(-1,1,2)
